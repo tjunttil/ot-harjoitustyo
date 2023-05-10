0df39f5d11425f6e01f49d7f45cc5e248657a3ea
Teemu Junttila <teemu.junttila@protonmail.com>
2023-05-10 16:26:00 +0300

    Lisätty kuvia dokumentaatiokansioon

--- src/build.py
+++ src/build.py
@@ -6,7 +6,6 @@ file_path = os.path.join(dirname,"..", "data", "points.csv")
 def build():
     with open(file_path, "w", encoding = "utf-8") as points_file:
         points_file.write("")
-    print(f"created points file in {file_path}")
 
 if __name__ == "__main__":
     build()
