c8217869f8f6c4a1ed132cd73355c50491043af6
tjunttil <teemu.junttila@protonmail.com>
2023-05-12 08:49:13 +0300

    Update asteroid.py

--- src/entities/sprites/asteroid.py
+++ src/entities/sprites/asteroid.py
@@ -4,7 +4,6 @@ class Asteroid(Entity):
     def __init__(self, coordinates, direction, velocity, size):
         super().__init__(coordinates, direction, velocity)
         super()._load_image("asteroid.png")
-        self.size = size
         self.__hits = 0
 
     def get_hit(self):
